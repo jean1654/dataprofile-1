bcd51f6d28d4b39e22774174b2a4102753307066
jean1654 <68804623+jean1654@users.noreply.github.com>
2025-06-06 08:23:10 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded",
 )
+
+pg = st.navigation([
+    # st.Page("pages/home.py", title="Home", default=True, icon=":material/home:"),
+    # st.Page("pages/homedataset.py", title="Dataset", icon=":material/dataset:"),
+    # st.Page("pages/datasetprofile.py", title="Dataset Profile Report", icon=":material/dataset:"),
+    st.Page("pages/validatephone.py", title="Validate Phone Numbers", icon=":material/dataset:"),
+])
+
+try:
+    pg.run()
+except Exception as e:
+   st.error(f"Something went wrong: {str(e)}", icon=":material/error:")
+
+st.sidebar.success("For more details, refer to Dataset tab.")
+
+# References below
+
+# st.set_page_config(page_title="Data Profile Reports", page_icon=":material/article:")
+
+# Widgets shared by all the pages
+# selected_data = st.sidebar.selectbox("Choose Preferred Dataset", ["Dataset1", "Dataset2", "Dataset3"], key="dataset")
+
+#def datasetpage():
+#    st.write(st.session_state.dataset)
